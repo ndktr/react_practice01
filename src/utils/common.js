b0118e627988e99ed109041b8ed56db7956ab493
ndktr <ndktr.git@gmail.com>
2023-06-09 11:45:32 +0900

    fix: add end time on todo card

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,6 +5,6 @@ export const formatDate = timestamp => {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
 
-    const formattedDate = `${year}-${month}-${day}`;
+    const formattedDate = `${year}/${month}/${day}`;
     return formattedDate;
 };
